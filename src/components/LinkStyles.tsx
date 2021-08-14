74858463795c0e9de559c906213f2eef9aa02c99
codeolam <frankuchenna30@gmail.com>
2021-08-14 15:07:29 +0100

    added Link handling

---
+++ src/components/LinkStyles.tsx
@@ -0,0 +1,83 @@
+import styled, { css } from 'styled-components';
+import { darken } from 'polished';
+
+import { color } from '../shared/styles';
+
+interface inStyle{
+    secondary: boolean,
+    tertiary: boolean,
+};
+
+export const linkStyles = css<inStyle>`
+  display: inline-block;
+  transition: transform 150ms ease-out, color 150ms ease-out;
+  text-decoration: none;
+
+  color: ${color.secondary};
+
+  &:hover,
+  &:focus {
+    cursor: pointer;
+    transform: translateY(-1px);
+    color: ${darken(0.07, color.secondary)};
+  }
+  &:active {
+    transform: translateY(0);
+    color: ${darken(0.1, color.secondary)};
+  }
+
+  svg {
+    display: inline-block;
+    height: 1em;
+    width: 1em;
+    vertical-align: text-top;
+    position: relative;
+    bottom: -0.125em;
+    margin-right: 0.4em;
+  }
+
+  ${props =>
+    props.secondary &&
+    css`
+      color: ${color.mediumdark};
+
+      &:hover {
+        color: ${color.dark};
+      }
+
+      &:active {
+        color: ${color.darker};
+      }
+    `};
+
+  ${props =>
+    props.tertiary &&
+    css`
+      color: ${color.dark};
+
+      &:hover {
+        color: ${color.darkest};
+      }
+
+      &:active {
+        color: ${color.mediumdark};
+      }
+    `};
+`;
+
+
+
+export const LinkA = styled.a`
+  ${linkStyles};
+`;
+
+export const applyStyle = LinkWrapper => {
+    return (
+      LinkWrapper &&
+      styled(({ containsIcon, inverse, nochrome, secondary, tertiary, ...linkWrapperRest }) => (
+        <LinkWrapper {...linkWrapperRest} />
+      ))`
+        ${linkStyles};
+      `
+    );
+  };
---
+++ src/shared/styles.ts
@@ -0,0 +1,14 @@
+import { css } from 'styled-components';
+
+export const color = {
+    // Palette
+    primary: '#FF4785', // coral
+    secondary: '#1EA7FD', // ocean
+    tertiary: '#DDDDDD',
+
+    // Monochrome
+    mediumdark: '#999999',
+    dark: '#666666',
+    darker: '#444444',
+    darkest: '#333333',
+  };
